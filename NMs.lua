0d5d8ab21dd85c7e1a897683379f6293dc8f22b2
FurryDT <dtarran@hotmail.com>
2024-09-14 20:30:07 +0100

    Initial Commit

---
+++ NMs.lua
@@ -0,0 +1,11 @@
+local NMs = T{ };
+NMs["ex"] = T{ };
+NMs["ex"]["PH"] = T{ };
+NMs["ex"]["PH"][1] = 29;
+NMs["ex"]["PH"][2] = 30;
+NMs["ex"]["ID"] = 31;
+NMs["ex"]["Respawn"] = 3600;
+NMs["ex"]["Time"] = 348;
+NMs["ex"]["Name"] = "Example";
+
+return NMs;
